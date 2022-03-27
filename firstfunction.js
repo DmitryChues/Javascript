c8bfead431886b2d0db59f9fcb2f9e29dd259fe5
DmitryChues <dmitrychues2312@gmail.com>
2022-03-27 16:44:19 +0300

    function

---
+++ firstfunction.js
@@ -0,0 +1,11 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOW WOOW");
+    } else {
+        console.log(name + " says woow woow");
+    }
+}
+bark("rover", 23);
+bark("spot", 13);
+bark("spike", 53);
+bark("lady", 17);
