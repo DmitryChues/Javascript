dc52826cd0ba7798cec4a9e8e614f3ccd15bd275
DmitryChues <dmitrychues2312@gmail.com>
2022-03-31 23:39:48 +0300

    RefactoringBubbles

---
+++ Bubbles2.js
@@ -0,0 +1,29 @@
+var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+    46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+
+function printAndGetHighScore(scores) {
+    var highScore = 0;
+    for (i = 0; i < scores.length; i++) {
+        console.log("Bubble solution #" + i + " score: " + scores[i]);
+        if (highScore < scores[i]) {
+            highScore = scores[i];
+        }
+    }
+    return highScore;
+}
+
+var highScore = printAndGetHighScore(scores);
+console.log("Bubble tests: " + scores.length)
+console.log("Highest bubble score: " + highScore);
+
+function getBestResults(scores, highScore) {
+    var best = [];
+    for (i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            best.push(i);
+        }
+    }
+    return best;
+}
+var bestSolution = getBestResults(scores, highScore);
+console.log("Solutions with the highest score: " + bestSolution);
